e0ca41fc8bc915155b5d5cd6ff671db0f3587ebb
Jayanthi-T <66419308+Jayanthi-T@users.noreply.github.com>
2021-03-28 16:34:21 +0530

    Data Structures MCQ 2

---
+++ Data Structures MCQ 2.c
@@ -0,0 +1,10 @@
+Which of the following data structures can handle updates and queries in log(n) time on an array?
+
+1) Linked list
+2) Stack
+3) Segment Tree
+4) Queue
+
+Answer:
+      The answer is Segment tree.
+      Please refer to this wikipedia article to read all the details of Segment Trees.
