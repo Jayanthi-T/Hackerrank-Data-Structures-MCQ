03f6f69bea6ae5d1655bdb115f88ed0e31084e30
Jayanthi-T <66419308+Jayanthi-T@users.noreply.github.com>
2021-03-28 16:32:34 +0530

    Data Structures MCQ 1

---
+++ Data Structures MCQ 1.c
@@ -0,0 +1,10 @@
+If we have a tree of n nodes, how many edges will it have?
+
+1) 1
+2) (n*(n-1))/2
+3) (n*(n-1))
+4) n-1
+
+Answer:
+      The answer is n-1.
+      Because if there are n nodes say,3 ,then there could be 2 edges.So,(n-1) edges.
