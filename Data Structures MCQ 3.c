656639ee76deec78b68b715c4cdc515dce1cc754
Jayanthi-T <66419308+Jayanthi-T@users.noreply.github.com>
2021-03-28 16:36:36 +0530

    Data Structures MCQ 3

---
+++ Data Structures MCQ 3.c
@@ -0,0 +1,11 @@
+Of the following data structures, which has a Last in First Out ordering? In other words, the one that
+came in last will be the first to be popped.
+
+1) Queue
+2) Stack
+3) Vector
+4) Array List
+
+Answer:
+      The answer is Stack.
+      Because Stack follows LIFO(Last In First Out) mechanism.
